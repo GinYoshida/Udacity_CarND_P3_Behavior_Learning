933f34182528f681d2109073bc5e0215050be56b
GinYoshida <wood1969@hotmail.com>
2018-01-03 15:39:45 +0000

    update

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import csv
 import cv2
+import numpy as np
 import matplotlib.pyplot as plt
 
 lines = []
@@ -18,5 +19,17 @@ for line in lines[1:3]:
     images.append(cv2.imread(current_path))
     measurements.append(float(line[3]))
 
-from keras.models import Sequentail
+X_train = np.array(images)
+y_train = np.array(measurements)
+
+from keras.models import Sequential
 from keras.layers import Flatten, Dense
+
+model = Sequential()
+model.add(Flatten(input_shape=(160,320,3)))
+model.add(Dense(1))
+
+model.compile(loss='mse', optimizer='adam')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True,nb_epoch=7)
+
+model.save('model.h5')
